4e5299baac882b67d2228eed5f3b30de0f921adc
小傅Fox <xfox@cotr.me>
2014-08-04 20:32:51 +0800

    new api arrangement init

--- back/api.php
+++ back/api.php
@@ -18,198 +18,31 @@ if (isset( $_REQUEST[ 'mod' ] ) && isset( $_REQUEST[ 'api' ] )) {
 switch ($call) {
     case 'timeline/public' :
         {
-            if ($_SERVER[ 'REQUEST_METHOD' ] != 'GET') {
-                throw new Exception( 'invaid method', 1 );
-            }
-            $result = iterator_to_array( $coll[ 'posts' ]->find()->sort( array (
-                    'time' => - 1 
-            ) )->limit( 20 ) );
-            $return[ 'posts' ] = array ();
-            foreach ( $result as $k => $v ) {
-                $author = iterator_to_array( $coll[ 'users' ]->find( array (
-                        '_id' => new MongoId( $v[ 'author' ] ) 
-                ) ) )[$v[ 'author' ]];
-                $Rauthor = array (
-                        'id' => $v[ 'author' ],
-                        'nick' => $author[ 'nick' ] 
-                );
-                $return[ 'posts' ][ ] = array (
-                        'id' => $k,
-                        'title' => $v[ 'title' ],
-                        'text_html' => MarkdownExtra::defaultTransform( $v[ 'text' ] ),
-                        'text_md' => $v[ 'text' ],
-                        'author' => $Rauthor,
-                        'time' => date( "Y-m-d H:i:s", $v[ 'time' ] ) 
-                );
-            }
-            echo json_encode( $return );
             break;
         }
     case 'auth/auth' :
         {
-            if ($_SERVER[ 'REQUEST_METHOD' ] != 'POST') {
-                throw new Exception( 'invaid method', 1 );
-            }
-            if ($coll[ 'users' ]->find( array (
-                    'nick' => $_REQUEST[ 'nick' ],
-                    'pass' => md5( $_REQUEST[ 'pass' ] ) 
-            ) )->count() > 0) {
-                $token = $coll[ 'users' ]->find( array (
-                        'nick' => $_REQUEST[ 'nick' ],
-                        'pass' => md5( $_REQUEST[ 'pass' ] ) 
-                ) )->getNext()['token'];
-                $return = array (
-                        'token' => $token 
-                );
-                echo json_encode( $return );
-            } else {
-                throw new Exception( 'nickname and password mismatch', 2 );
-            }
             break;
         }
     case 'post/show' :
         {
-            
-            if ($_SERVER[ 'REQUEST_METHOD' ] != 'GET') {
-                throw new Exception( 'invaid method', 1 );
-            }
-            $post = $coll[ 'posts' ]->find( array (
-                    '_id' => new MongoId( $_REQUEST[ 'id' ] ) 
-            ) )->limit( 1 )->getNext();
-            if (! $post) {
-                throw new Exception( 'invaid id', 4 );
-            }
-            $author = $coll[ 'users' ]->find( array (
-                    '_id' => new MongoId( $post[ 'author' ] ) 
-            ) )->getNext();
-            $return = array (
-                    'id' => $_REQUEST[ 'id' ],
-                    'title' => $post[ 'title' ],
-                    'author' => array (
-                            'id' => $post[ 'author' ],
-                            'nick' => $author[ 'nick' ] 
-                    ),
-                    'text_html' => MarkdownExtra::defaultTransform( $post[ 'text' ] ),
-                    'text_md' => $post[ 'text' ],
-                    'time' => date( "Y-m-d H:i:s", $post[ 'time' ] ) 
-            );
-            echo json_encode( $return );
             break;
         }
     case 'post/post' :
         {
-            if ($_SERVER[ 'REQUEST_METHOD' ] != 'POST') {
-                throw new Exception( 'invaid method', 1 );
-            }
-            $author = $coll[ 'users' ]->find( array (
-                    'token' => $_REQUEST[ 'token' ] 
-            ) )->getNext();
-            if ($author) {
-                $ins = array (
-                        'title' => $_REQUEST[ 'title' ],
-                        'text' => $_REQUEST[ 'text' ],
-                        'author' => (string) ($author[ '_id' ]),
-                        'time' => time() 
-                );
-                $result = $coll[ 'posts' ]->insert( $ins );
-                if ($result) {
-                    $id = (string) $ins[ '_id' ];
-                    // show
-                    $post = $coll[ 'posts' ]->find( array (
-                            '_id' => new MongoId( $id ) 
-                    ) )->limit( 1 )->getNext();
-                    if (! $post) {
-                        throw new Exception( 'invaid id', 4 );
-                    }
-                    $author = $coll[ 'users' ]->find( array (
-                            '_id' => new MongoId( $post[ 'author' ] ) 
-                    ) )->getNext();
-                    $return = array (
-                            'id' => $id,
-                            'title' => $post[ 'title' ],
-                            'author' => array (
-                                    'id' => $post[ 'author' ],
-                                    'nick' => $author[ 'nick' ] 
-                            ),
-                            'text_html' => MarkdownExtra::defaultTransform( $post[ 'text' ] ),
-                            'text_md' => $post[ 'text' ],
-                            'time' => $post[ 'time' ] 
-                    );
-                    echo json_encode( $return );
-                } else {
-                    throw new Exception( 'server error', 0 );
-                }
-            } else {
-                throw new Exception( 'invaid token', 5 );
-            }
             break;
         }
     case 'user/show' :
         {
-            if ($_SERVER[ 'REQUEST_METHOD' ] != 'GET') {
-                throw new Exception( 'invaid method', 1 );
-            }
-            $result = iterator_to_array( $coll[ 'posts' ]->find( array (
-                    'author' => $_REQUEST[ 'id' ] 
-            ) )->sort( array (
-                    'time' => - 1 
-            ) )->limit( 20 ) );
-            $return[ 'posts' ] = array ();
-            $posts_c = 0;
-            foreach ( $result as $k => $v ) {
-                $author = iterator_to_array( $coll[ 'users' ]->find( array (
-                        '_id' => new MongoId( $v[ 'author' ] ) 
-                ) ) )[$v[ 'author' ]];
-                $Rauthor = array (
-                        'id' => $v[ 'author' ],
-                        'nick' => $author[ 'nick' ] 
-                );
-                $return[ 'posts' ][ ] = array (
-                        'id' => $k,
-                        'title' => $v[ 'title' ],
-                        'text_html' => MarkdownExtra::defaultTransform( $v[ 'text' ] ),
-                        'text_md' => $v[ 'text' ],
-                        'author' => $Rauthor,
-                        'time' => date( "Y-m-d H:i:s", $v[ 'time' ] ) 
-                );
-                $posts_c ++;
-            }
-            $return[ 'id' ] = $_REQUEST[ 'id' ];
-            $return[ 'post_count' ] = $posts_c;
-            $return[ 'nick' ] = $Rauthor[ 'nick' ];
-            echo json_encode( $return );
             break;
         }
     case 'user/reg' :
         {
-            if ($_SERVER[ 'REQUEST_METHOD' ] != 'POST') {
-                throw new Exception( 'invaid method', 1 );
-            }
-            if ($coll[ 'users' ]->find( array (
-                    'nick' => $_REQUEST[ 'nick' ] 
-            ) )->count() > 0) {
-                throw new Exception( 'nickname already used', 3 );
-            } else {
-                $result = $coll[ 'users' ]->insert( array (
-                        'nick' => $_REQUEST[ 'nick' ],
-                        'pass' => md5( $_REQUEST[ 'pass' ] ),
-                        'token' => md5( $_REQUEST[ 'nick' ] . $_REQUEST[ 'pass' ] ) 
-                ) );
-                if ($result) {
-                    $return = array (
-                            'nick' => $_REQUEST[ 'nick' ],
-                            'token' => md5( $_REQUEST[ 'nick' ] . $_REQUEST[ 'pass' ] ) 
-                    );
-                    echo json_encode( $return );
-                } else {
-                    throw new Exception( 'server error', 0 );
-                }
-            }
             break;
         }
     default :
         {
-            throw new Exception( 'invaid api call', 6 );
+            throw new ApiEx( 'invaid api call', 6 );
             break;
         }
 }
--- back/functions/api.php
+++ back/functions/api.php
@@ -1,10 +1,13 @@
 <?php
-function api_result($method, $function, $data = array()){
+function api_result($method, $function){
     try {
         if ($_SERVER[ 'REQUEST_METHOD' ] != $method) {
             throw new ApiEx( 'invaid method', 1 );
         }
-        $r = json_encode( $function( $data ) );
+        $args = func_get_args();
+        array_shift($args);
+        array_shift($args);
+        $r = json_encode( callback($function, $args) );
         if (json_last_error()) {
             throw new Exception( json_last_error_msg(), json_last_error() );
         }
@@ -30,5 +33,14 @@ function api_result($method, $function, $data = array()){
             throw new Exception( json_last_error_msg(), json_last_error() );
         }
         echo $r;
+    }
+}
+
+function callback($function,$args)
+{
+    if (is_callable($function)) {
+        call_user_func_array($function, $args);
+    }else{
+        throw new Exception('invaid function param');
     }
 }
