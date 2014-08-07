16b71d7001edc6390da57dc88aa4f64c752b4efa
小傅Fox <xfox@cotr.me>
2014-08-07 13:29:21 +0800

    class User& inc.php& new config

---
+++ back/classes/user.php
@@ -0,0 +1,74 @@
+<?php
+class User{
+    /**
+     * get: getData
+     * set: setData
+     * 
+     * @var MongoCursor
+     */
+    private $data;
+    /**
+     * construct by data
+     * 
+     * @param MongoCursor $data            
+     */
+    public function __construct($data){
+        $this->setData( $data );
+    }
+    /**
+     * construct by id
+     * 
+     * @param mixed $id
+     *            string/MongoId
+     * @return User
+     */
+    public static function byId($id){
+        if (get_type( $id ) == 'MongoId') {
+            $query[ '_id' ] = $id;
+        } elseif (get_type( $id ) == 'string') {
+            $query[ '_id' ] = new MongoId( $id );
+        } else {
+            API::error( new Exception( 'invaid value' ) );
+        }
+        return new User( $coll[ 'users' ]->find( $query )->limit( 1 ) );
+    }
+    /**
+     * get: data
+     * 
+     * @return MongoCursor
+     */
+    public function getData(){
+        return $this -> data;
+    }
+    /**
+     * set: data
+     * 
+     * @param MongoCursor $data            
+     */
+    private function setData($data){
+        if (get_type( $data ) != 'MongoCursor') {
+            API::error( new PHPEx( 'invaid value' ) );
+        } else {
+            if ($data->count() == 1) {
+                $this -> data = $data;
+            } else {
+                $this -> data = $data->limit( 1 );
+            }
+        }
+    }
+    /**
+     * get iterator_to_array of the data
+     *
+     * @return array
+     */
+    private function getRawArray(){
+        return current( iterator_to_array( $this -> data ) );
+    }
+    public function getArray(){
+        $user = $this->getRawArray();
+        return array (
+                'id' => (string) $user[ '_id' ],
+                'nick' => $user[ 'nick' ] 
+        );
+    }
+}
--- back/inc.php
+++ back/inc.php
@@ -4,10 +4,12 @@ include_once __DIR__ . '/md/Michelf/Markdown.inc.php';
 include_once __DIR__ . '/md/Michelf/MarkdownExtra.inc.php';
 // classes
 include_once __DIR__ . '/classes/apiex.php';
+include_once __DIR__ . '/classes/phpex.php';
+include_once __DIR__ . '/classes/api.php';
+include_once __DIR__ . '/classes/post.php';
+include_once __DIR__ . '/classes/user.php';
 // functions
-include_once __DIR__ . '/functions/user.php';
-include_once __DIR__ . '/functions/post.php';
-include_once __DIR__ . '/functions/api.php';
+include_once __DIR__ . '/functions/extend.php';
 // config
 if (file_exists( __DIR__ . '/config.json' )) {
     $config = fopen( __DIR__ . '/config.json', 'r' );
